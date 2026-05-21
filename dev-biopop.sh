40410663210cf74c688c09faa8cc672e65df7b9f
Madhava Jay <me@madhavajay.com>
2026-05-21 16:51:12 +1000

    fixes

--- dev-biopop.sh
+++ dev-biopop.sh
@@ -8,9 +8,29 @@ export BIOPOP_DATA_DIR="${BIOPOP_DATA_DIR:-$BIOPOP_ROOT/01_mock_data_generation/
 export BIOPOP_FACETS_FILE="${BIOPOP_FACETS_FILE:-$BIOPOP_ROOT/01_mock_data_generation/facets/biovault-facets.csv}"
 export BIOPOP_FLOW_URL_ROOT="${BIOPOP_FLOW_URL_ROOT:-https://github.com/madhavajay/BioVault_popgen/tree/main/flows}"
 
+DEVSTACK_RESET="${DEVSTACK_RESET:-0}"
+forward_args=()
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+		--reset)
+			DEVSTACK_RESET=1
+			shift
+			;;
+		--no-reset)
+			DEVSTACK_RESET=0
+			shift
+			;;
+		*)
+			forward_args+=("$1")
+			shift
+			;;
+	esac
+done
+export DEVSTACK_RESET
+
 args=(--biopop)
 if [[ "${BIOPOP_INTERACTIVE:-1}" == "1" ]]; then
 	args+=(--interactive)
 fi
 
-exec "$SCRIPT_DIR/test-scenario.sh" "${args[@]}" "$@"
+exec "$SCRIPT_DIR/test-scenario.sh" "${args[@]}" "${forward_args[@]}"
